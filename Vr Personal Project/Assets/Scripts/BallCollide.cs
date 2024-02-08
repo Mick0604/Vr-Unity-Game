d4fa9109d6a88a32c17d554d43082f46007d2360
Mick0604 <73721595+Mick0604@users.noreply.github.com>
2024-02-08 12:15:51 -0500

    2/8/24

---
+++ Vr Personal Project/Assets/Scripts/BallCollide.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCollide : MonoBehaviour
+{
+    private int bounceCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other) {
+        /*bounceCount++;
+
+        if (bounceCount < 2)
+        {
+            gameObject.transform.position = new Vector3(25f, 0f, 0f);
+
+
+        }
+        */
+    }
+}
